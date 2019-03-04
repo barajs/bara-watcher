a3d0a94f1380b6975c7d990c463c13f011ddd686
Glitch (hello-express) <none>
2019-03-04 14:39:47 +0000

    🎷🦂 Checkpoint ./package.json:587926/39 ./server.js:587926/2031

--- server.js
+++ server.js
@@ -1,22 +1,45 @@
-// server.js
-// where your node app starts
+const {register, useStream, useTrigger, useEvent, useCondition, useAction} = require('bara');
+ 
+const ON_TIME_ESLAPSED = 'ON_TIME_ESLAPSED';
+ 
+const EVERY_X_SECOND = (x) => (triggeringEvent) => triggeringEvent.payload % x === 0;
+const ONLY_EVEN_SECOND = (triggeringEvent) => triggeringEvent.payload % 2 === 0;
 
-// init project
-const express = require('express');
-const app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+const timeElapsedStream = () => {
+  return {
+    name: 'Tik Tok',
+    eventTypes: [ON_TIME_ESLAPSED],
+    setup: ({emit}) => {
+      let elapsed = 0;
+      const timer = setInterval(() => {
+        emit(ON_TIME_ESLAPSED, elapsed++);
+      }, 1000);
+    },
+  }
+}
+ 
+const everyTwoSecondsTrigger = {
+  name: 'Console Every Two Seconds',
+  event: useEvent(ON_TIME_ESLAPSED),
+  condition: useCondition(ONLY_EVEN_SECOND),
+  action: useAction(({payload}) => {
+    console.log(`Console every two seconds: ${payload}`);
+  }),
+};
+ 
+const everyXSecondsTrigger = {
+  name: 'Console Every 20 Seconds',
+  event: useEvent(ON_TIME_ESLAPSED),
+  condition: useCondition(EVERY_X_SECOND(5)),
+  action: useAction(({payload}) => {
+    console.log(`Console every 5 seconds: ${payload}`);
+  }),
+};
+ 
+const tikTokApp = () => {
+    useStream(timeElapsedStream);
+    useTrigger(everyTwoSecondsTrigger);
+    useTrigger(everyXSecondsTrigger)
+}
+ 
+register(tikTokApp);
