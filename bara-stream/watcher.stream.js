1ffbc7989c7181e5727a8d1d278fe888d80b4ff4
Glitch (hello-express) <none>
2019-04-01 03:02:24 +0000

    📚🌌 Checkpoint ./bara-stream/watcher.stream.js:587926/268 ./package.json:587926/13 ./watcher.js:587926/607

--- bara-stream/watcher.stream.js
+++ bara-stream/watcher.stream.js
@@ -1,9 +1,16 @@
+const path = require('path');
 const chokidar = require('chokidar');
-const {ON_FILE_ADDED, ON_} = require('./watcher.event');
+const {ON_FILE_ADDED, ON_FILE_DELETED} = require('./watcher.event');
 
 const watcherStream = () => {
   return {
     name: 'Bara Watcher',
+    eventTypes: [ON_FILE_ADDED, ON_FILE_DELETED],
+    setup: ({emit}) => {
+      const watchDir = path.resolve(__dirname, 'examples');
+      console.log(watchDir);
+      const 
+    }
   }
 }
 
--- watcher.js
+++ watcher.js
@@ -1,5 +1,20 @@
-const {useStream} = require('bara');
-const {} = require('./bara-stream/watcher.stream')
+const {register, useStream, useEvent, useAction, useTrigger} = require('bara');
+const {watcherStream} = require('./bara-stream/watcher.stream');
+const {ON_FILE_ADDED} = require('./bara-stream/watcher.event');
 
-function main() {
+
+const fileTrigger = {
+  name: 'File TXT Trigger',
+  event: useEvent(ON_FILE_ADDED),
+  action: useAction((data) => {
+    console.log(data);
+  })
 }
+
+function fsWatcher() {
+  useStream(watcherStream());
+  useTrigger(fileTrigger);
+  console.log(`Registered success!`);
+}
+
+register(fsWatcher);
